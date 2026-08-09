8f55f4d1ae07718039af4fae00c9701e69dea41d
Uzma Yakub <uzmayakub27@gmail.com>
2026-08-09 19:43:06 +0600

    Delete src directory

--- src/components/EmptyState.tsx
+++
@@ -1,7 +0,0 @@
-export function EmptyState({ message }: { message: string }) {
-  return (
-    <div className="mt-10 rounded-2xl border border-dashed border-maroon-200 bg-white/60 p-10 text-center text-sm text-maroon-900/60">
-      {message}
-    </div>
-  );
-}
